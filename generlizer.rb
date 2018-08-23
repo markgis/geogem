7c7465ed1bbf728ebbd15ada701df4052c59fc8d
markgis <mark.allen@groundsure.com>
2018-08-23 17:15:48 +0100

    started generalizer

--- generlizer.rb
+++ generlizer.rb
@@ -3,12 +3,42 @@ require "cmath"
 
 class Generlizer
 
-  def distance(x,y,x0,y0)
-    return CMath.sqrt((x-x0)**2 + (y-y0)**2)
-end
-  def radial_thin(nodes, tolerance)
-     node_split = nodes.split(",")[1..-2]
-     puts node_split
-     puts node_split.each_cons(2).map {|a,b| a[0] == b[0]}
+  def distance(x, y, x0, y0)
+    CMath.sqrt((x - x0)**2 + (y - y0)**2)
   end
+
+  def first_node(nodes)
+    nodes.strip.split(',').first.strip
+  end
+
+  def last_node(nodes)
+    nodes.split(',').last.strip
+  end
+
+  def node_split(nodes)
+    node_split = nodes.split(',')[1..-2]
+    node_split.map do |node|
+      split_node = node.strip!.split
+      { x: split_node[0].to_i, y: split_node[1].to_i }
+    end
+  end
+
+  def node_distance(nodes)
+    points_distance = []
+    points = node_split(nodes)
+    points.each_with_index do |point, index|
+      if (index + 1) < points.length
+      dist = distance(point[:x], point[:y], points[index + 1][:x], points[index + 1][:y])
+      else
+        dist = -1
+      end
+      points_distance.push(dist)
+    end
+    points_distance
+  end
+
+  def radial_thin(nodes, tolerance)
+    distance = node_distance(nodes)
+  end
+
 end
--- spec/generlizer_spec.rb
+++ spec/generlizer_spec.rb
@@ -4,11 +4,36 @@ RSpec.describe Generlizer do
   describe 'distance' do
     it 'returns distance from two points' do
       expect(Generlizer.new.distance(400000,200000,400100,200000)).to eq(100)
-end
-  # describe 'node_thin' do
-  #   it 'removes nodes that are closer then tolerance' do
-  #     expect(Generlizer.new.radial_thin('0 1, 0 2, 0 4, 1 4, 2 4, 4 4', 1)).to eq('0 1, 0 4, 1 4, 4 4')
-  #   end
-  # end
-end
+    end
+  end
+
+  describe 'node_split' do
+    it 'returns nodes split as array of objects' do
+      expect(Generlizer.new.node_split('0 1, 0 2, 0 4, 1 4, 2 4, 4 4')).to eq([{:x=>0, :y=>2}, {:x=>0, :y=>4}, {:x=>1, :y=>4}, {:x=>2, :y=>4}])
+    end
+
+  describe'node_distance' do
+    it 'returns distance between nodes' do
+      expect(Generlizer.new.node_distance('0 1, 0 2, 0 4, 1 4, 2 4, 4 4')).to eq([2.0, 1.0, 1.0, -1])
+    end
+  end
+
+  describe 'first_node' do
+    it 'returns the first node x and y from string' do
+      expect(Generlizer.new.first_node('0 1, 0 2, 0 4, 1 4, 2 4, 4 4')).to eq('0 1')
+    end
+  end
+
+  describe 'last_node' do
+    it 'returns the last node x and y from string' do
+      expect(Generlizer.new.last_node('0 1, 0 2, 0 4, 1 4, 2 4, 4 4')).to eq('4 4')
+    end
+  end
+
+  describe 'node_thin' do
+    it 'removes nodes that are closer then tolerance' do
+      expect(Generlizer.new.radial_thin('0 1, 0 2, 0 4, 1 4, 2 4, 4 4', 1)).to eq('0 1, 0 4, 1 4, 4 4')
+    end
+  end
+  end
 end
