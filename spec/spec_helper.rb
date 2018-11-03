b49508361c95541059b99ae791095b8fd10bd86f
Mike Harris <mike.harris@groundsure.com>
2018-11-03 18:11:30 +0000

    Added douglas-peucker algorithm, including basic spec test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,5 +2,6 @@ require_relative '../geogem.rb'
 require_relative '../generalization/radial_thin.rb'
 require_relative '../generalization/perpend_distance.rb'
 require_relative '../validation/geom_check.rb'
+require_relative '../generalization/douglas-peucker.rd'
 
 require 'yaml'
