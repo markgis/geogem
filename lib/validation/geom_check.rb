39dd6581c6c7ca5df576ba2cef97a451c9b68612
markgis <mark.allen@groundsure.com>
2018-11-21 17:46:27 +0000

    slight re factor, only start on validate

--- lib/validation/geom_check.rb
+++ lib/validation/geom_check.rb
@@ -1,13 +1,11 @@
 include Math
-require "cmath"
-
+require 'cmath'
 
 class GeomCheck
-
   def slope(point1, point2)
     dy = Float(point2[1] - point1[1])
     dx = Float(point2[0] - point1[0])
-    return 0.0 if dy == 0
+    return 0.0 if dy.zero?
     dy / dx
   end
 end
