cfbccf7bb1ff8d10ae573dd2ab8a17d7863c3faf
joshkey-gs <joshua.keys@groundsure.com>
2018-11-21 15:37:29 +0000

    Switch slope method for atan and added radians to degrees conversion

--- spec/geom_check_spec.rb
+++ spec/geom_check_spec.rb
@@ -1,9 +1,17 @@
 require_relative 'spec_helper'
 
 RSpec.describe GeomCheck do
-  describe 'slope' do
-    it 'checks angle from two points' do
-      expect(GeomCheck.new.slope([1,2],[3,4])).to eq(4)
+
+  describe 'radians' do
+    it 'gets the bearing in radians between two points' do
+      expect(GeomCheck.new.radians([1,2],[3,4])).to eq(0.7853981633974483)
     end
   end
+
+  describe 'bearing' do
+    it 'gets the bearing in degrees between two points' do
+      expect(GeomCheck.new.bearing([1,2],[3,4])).to eq(44.999999999999999449063216305326)
+    end
+  end
+
 end
--- validation/geom_check.rb
+++ validation/geom_check.rb
@@ -4,10 +4,15 @@ require "cmath"
 
 class GeomCheck
 
-  def slope(point1, point2)
+  def radians(point1, point2)
     dy = Float(point2[1] - point1[1])
     dx = Float(point2[0] - point1[0])
-    return 0.0 if dy == 0
-    dy / dx
+    atan(dy/dx)
   end
+
+  def bearing(point1, point2)
+    radians = radians(point1,point2)
+    (180/Math::PI)*radians
+  end
+
 end
