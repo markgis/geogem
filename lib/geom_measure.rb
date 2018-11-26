e9ff6f6f56462206bebb55d562e1a174810ebb52
markgis <mark.allen@groundsure.com>
2018-11-26 16:56:36 +0000

    started to move functions into geom_measure, area needs to be moved to, line length etc could also go here

---
+++ lib/geom_measure.rb
@@ -0,0 +1,19 @@
+include Math
+require 'cmath'
+
+class GeomMeasure
+
+  def radians(point1, point2)
+    dy = Float(point2.y - point1.y)
+    dx = Float(point2.x - point1.x)
+    radians = atan2(dx,dy)
+    return radians unless radians < 0
+    radians+(2*Math::PI)
+  end
+
+  def bearing(point1, point2)
+    radians = radians(point1,point2)
+    (180/Math::PI)*radians
+  end
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,5 +3,6 @@ require_relative '../lib/generalization/radial_thin.rb'
 require_relative '../lib/generalization/perpend_distance.rb'
 require_relative '../lib/validation/geom_check.rb'
 require_relative '../lib/generalization/douglas-peucker.rb'
+require_relative '../lib/geom_measure.rb'
 
 require 'yaml'
